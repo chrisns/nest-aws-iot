34713eba8eb6c55788e776df24e6adae8298b32d
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-01-07 10:46:47 +0000

    only look for delta on the shadow

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const Cylon = require('cylon')
 const camelCase = require('camelcase')
 const AWSMqtt = require("aws-mqtt-client").default
 
-const { AWS_IOT_ENDPOINT_HOST, ACCESS_TOKEN, DEVICE_ID} = process.env
+const { AWS_IOT_ENDPOINT_HOST, ACCESS_TOKEN, DEVICE_ID } = process.env
 
 const awsMqttClient = new AWSMqtt({
   endpointAddress: AWS_IOT_ENDPOINT_HOST,
   logger: console
 })
 
-awsMqttClient.on("connect", () => awsMqttClient.subscribe([`$aws/things/nest_${DEVICE_ID}/shadow/update/accepted`],
+awsMqttClient.on("connect", () => awsMqttClient.subscribe([`$aws/things/nest_${DEVICE_ID}/shadow/update/delta`],
   { qos: 1 },
   (err, granted) => console.log("aws", err, granted)
 ))
+
+awsMqttClient.on("error", (error) => console.log(error))
 
 Cylon.robot({
   connections: {
@@ -20,12 +22,12 @@ Cylon.robot({
   },
 
   devices: {
-    thermostat: { driver: 'nest-thermostat', connection:'nest',  deviceId:DEVICE_ID },
+    thermostat: { driver: 'nest-thermostat', connection: 'nest', deviceId: DEVICE_ID },
   },
 
   work: function (my) {
-    my.thermostat.on('status', data => 
-      awsMqttClient.publish(`$aws/things/nest_${DEVICE_ID}/shadow/update`, JSON.stringify({state: {reported: data}}), { qos: 0 })
+    my.thermostat.on('status', data =>
+      awsMqttClient.publish(`$aws/things/nest_${DEVICE_ID}/shadow/update`, JSON.stringify({ state: { reported: data } }), { qos: 0 })
     )
 
     awsMqttClient.on("message", (topic, message) => {
@@ -40,7 +42,7 @@ Cylon.robot({
           console.error(`${key} set to ${value} failed`)
         }
       })
-      return awsMqttClient.publish(`$aws/things/nest_${DEVICE_ID}/shadow/update`, JSON.stringify({state: {desired: null}}), { qos: 0 })
+      return awsMqttClient.publish(`$aws/things/nest_${DEVICE_ID}/shadow/update`, JSON.stringify({ state: { desired: null } }), { qos: 0 })
     })
   }
 }).start()
